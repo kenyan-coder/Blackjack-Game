eebcd49565cab5fd78776819879f22c0db98d523
kenyan-coder <scamsours@gmail.com>
2021-07-04 19:01:22 +0300

    remove bug in duplicate js script

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 let firstCard = 10
 let secondCard = 4
 let sum = firstCard + secondCard
@@ -5,19 +6,20 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
+let messageEl = document.getElementById("message-el");
+
 // create a function to start the game
-function startGame(){
+function startGame() {
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card? "
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
-    }    
-    console.log(message)
+    }
+    //Display the message to the user.  
+    messageEl.textContent = message;
 
 }
-
-
