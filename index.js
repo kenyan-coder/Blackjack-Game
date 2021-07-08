a97040dbb565563662be241588572c003d20dd3e
kenyan-coder <scamsours@gmail.com>
2021-07-08 16:34:01 +0300

    render player details

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+let player ={
+    name:"Per Borgen",
+    chips:"$145"
+}
 
 //array of ordered cards
 let cards = []
@@ -10,6 +13,11 @@ let message = ""
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el")
 let cardsEl=document.getElementById("cards-el")
+let playerEl=document.getElementById("player-el")
+
+// 4. Render the player's name and chips in playerEl
+playerEl.textContent=player.name+ ": " +player.chips;
+
 function getRandomCard(){
     let randomCard= Math.floor(Math.random()*13) +1 
     if (randomCard > 10){
@@ -24,6 +32,7 @@ function getRandomCard(){
 
 // create a function to start the game
 function startGame(){
+    isAlive=true
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
     cards = [firstCard,secondCard]
